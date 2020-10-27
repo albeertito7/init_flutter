c8044026f2bb8a2ee657d1a956e567e3b0be70ee
Albert <albertperezdatsira@gmail.com>
2020-10-27 17:18:40 +0100

    first test program

---
+++ firstProgram.dart
@@ -0,0 +1,41 @@
+// DART
+
+// main function (entrance), returning void
+void main() {
+  for (int i = 1; i <= 5; i++) {
+    print('Hello World ${i}');
+  }
+
+  //statically typed
+  int age = 30; //we can't change dinamically its type
+  print(age);
+
+  String s = "dart";
+  print(s);
+
+  bool isNight = false;
+  print(isNight);
+
+  //using dynamic types
+  dynamic name = "dart";
+  name = 30;
+  print(name);
+
+  //type int, using var the type is interpreted by its value
+  var test = 20; // we cannot modify its type dynamically, as before
+  print(test);
+
+  String greet = greeting();
+  age = getAge();
+
+  print(age);
+  print(greet);
+}
+
+// returning diferent types inside functions
+String greeting() {
+  return 'Greetings, my friends!';
+}
+
+// we can convert the funciont into a arrow function
+int getAge() => 24;
