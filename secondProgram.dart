241af4127f88475bb588293f77f6f1668d0b01fb
Albert <albertperezdatsira@gmail.com>
2020-10-27 17:53:10 +0100

    added list examples

--- secondProgram.dart
+++ secondProgram.dart
@@ -1,1 +1,20 @@
-void main() {}
+void main() {
+  // creation of a list
+  List names = ['vegeta', 'arale', 'frieza']; // init putting values inside
+  print(names);
+
+  // using methods we can act on the list
+  names.add("natsu");
+  print(names);
+
+  names.remove('frieza');
+  print(names);
+
+  // because its not a typed list, we can add other kind of vars
+  // is not set what kind of datatype is going inside
+  names.add(23);
+  print(names);
+
+  List<int> ages = [1, 2, 3, 4];
+  print(ages);
+}
