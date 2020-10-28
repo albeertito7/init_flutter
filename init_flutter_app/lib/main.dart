d3850efc104a14400ebe081bdff94144a5cdd58e
Albert <albertperezdatsira@gmail.com>
2020-10-28 17:27:59 +0100

    body added for main screen and some comments

--- init_flutter_app/lib/main.dart
+++ init_flutter_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
+
   runApp(MaterialApp(
     theme: ThemeData(
       primarySwatch: Colors.blue,
@@ -11,7 +12,31 @@ void main() {
         title: Text("my first app"),
         centerTitle: true,
       ),
-      body: Text('hello my friends'),
+      body: Center(
+        child: Text('hello my friends'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Center(
+          child: Text('click'),
+        )
+      ) ,
     ),
   ));
 }
+
+/* Scaffold Widget
+* for general layouts of our apps
+* is a wrapper for many widgets
+* will expand and ocuppy the whole device screen, it will ocuppy the available space
+* will provide a framework to implement the basic material design layout of the applications
+*/
+
+/* key:
+* property to set and identifier for widgets, elements and semanticNodes
+* all the elements have this property
+* must be unique between the father's elements
+* */
+
+/* body:
+* to set the main widget for the screen
+* */
